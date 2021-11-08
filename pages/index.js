8a58366e432e3e8475c3683705accae76ac6a5d9
Graham Roberts <grahamroberts31@gmail.com>
2021-11-08 09:37:21 -0700

    Adds log to test firebase being read on vercel deploy

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import NavBar from '../components/navbar'
 import Brand from './../components/branding'
 
 export default function Home() {
-  
+  console.log(firebase);
   return (
      <div>
        <NavBar/>
