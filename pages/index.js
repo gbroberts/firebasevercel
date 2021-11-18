f6634586a494c31dc3e249c758026996073709c2
Graham Roberts <grahamroberts31@gmail.com>
2021-11-18 10:04:15 -0700

    Adds last class's work

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import firebase from '../firebase'
+import Button from '../components/ui/button'
+import GoogleProvider from '../components/auth/provider/google'
 
 import NavBar from '../components/navbar'
 import Brand from './../components/branding'
@@ -10,6 +12,15 @@ export default function Home() {
        <NavBar/>
        <Brand title="Home Page" tagline="directory app start"/>
 
+      <Button bgcolor="tomato" color="white">
+        {/* children */}
+        sign up with firebase
+      </Button>
+
+      <GoogleProvider>sign up with google</GoogleProvider>
+
+      {/* <Button bgcolor="blue" color="white">sign up with something</Button>
+      <Button bgcolor="orange" color="white">sign up with something</Button> */}
      </div>
      
        
